e5a4313533b37cda332df6a30f2ab3968622ce22
Dennis Corsi <denniscorsi@gmail.com>
2024-04-24 12:04:04 -0400

    Setup initial files with Vite

---
+++ notes.md
@@ -0,0 +1,3 @@
+API call format:
+
+https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=cancer&retmax=100
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+// Initialize express app
+const app = express();
+app.use(bodyParser.json());
+const PORT = 3000;
+
+// Catch-all endpoint to send a 404 status
+app.use("*", (req, res) => {
+  res.sendStatus(404);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultError = {
+    log: "Express error handler caught an unknown middleware error",
+    status: 500,
+    message: "Internal Server Error"
+  };
+  const error = Object.assign({}, defaultError, err);
+  res.status(error.status).json(error.message);
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
